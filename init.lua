b921c23810000d58db2fa069c12009a408eb05f5
ezequieljm <ezequieljm020@gmail.com>
2021-09-12 13:15:51 -0300

    Modif init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require("settings")
-require("keymappings")
--- require("coc")
+require("mappings")
+require("coc-config")
 
--- vim.g.colors_name = "sonokai"
--- vim.g["airline#extensions#tabline#enabled"] = 1
--- vim.g["airline_powerline_fonts"] = 1
--- vim.g["airline_theme"] = "sonokai"
--- vim.g["NERDTreeDirArrowExpandable"] = ""
--- vim.g["NERDTDirArrowCollapsible"] = ""
+vim.g.colors_name = "sonokai"
+vim.g["airline#extensions#tabline#enabled"] = 1
+vim.g["airline_powerline_fonts"] = 1
+vim.g["airline_theme"] = "sonokai"
+vim.g["NERDTreeDirArrowExpandable"] = ""
+vim.g["NERDTDirArrowCollapsible"] = ""
 
