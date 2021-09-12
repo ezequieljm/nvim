364c98a1b8559d1989ee1147648540ef1951a0df
ezequieljm <75186985+ezequieljm@users.noreply.github.com>
2021-09-12 13:49:36 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -22,5 +22,3 @@ vim.g.colors_name = "sonokai"
 vim.g["airline#extensions#tabline#enabled"] = 1
 vim.g["airline_powerline_fonts"] = 1
 vim.g["airline_theme"] = "sonokai"
-vim.g["NERDTreeDirArrowExpandable"] = ""
-vim.g["NERDTDirArrowCollapsible"] = ""
